e7a3c72ff7fb5444e78a6e5eb613aa624a983ed0
Katerina jser <kmalceva148@gmail.com>
2024-11-30 20:04:34 +0200

    Add all project files

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+    </section>
+  );
+;}
+
+export default About;
